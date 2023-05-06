6e9476f8295e95dfc9f0676ae96270e603c63f37
kadarisman <yuzarqadariesman@gmail.com>
2023-05-06 23:45:06 +0700

    fix config vercel.json dir

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Routers = require('../routers/index');
+const Routers = require('./routers/index');
 const dotenv       = require('dotenv');
 dotenv.config();
 const cors          = require('cors');
